d1d356a89ceabe2a425760ada4b8612f0e833fb9
ayn-1234 <91061060+ayn-1234@users.noreply.github.com>
2024-01-06 00:26:50 -0500

    Create taqueria.py

---
+++ Week 3/taqueria.py
@@ -0,0 +1,39 @@
+def main():
+    menu_asker()
+
+def menu_asker():
+    menu = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+    }
+
+    total = 0
+
+    while True:
+        try:
+            item = input("Item: ")
+            item = item.title()
+
+            if item in menu:
+                total += menu[item]
+            else:
+                continue
+
+        except EOFError:
+            print("\n")
+            break
+
+        else:
+            print(f"Total: ${total:.2f}")
+
+
+
+
+main()
