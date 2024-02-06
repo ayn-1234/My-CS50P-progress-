b5da115f9f138a8228fbf0e0ade6f7a547f08993
Ayan Ali Rudro <91061060+ayn-1234@users.noreply.github.com>
2024-02-06 09:03:55 -0500

    Create watch.py

---
+++ Week 7/watch.py
@@ -0,0 +1,16 @@
+import re
+import sys
+
+
+def main():
+    print(parse(input("HTML: ")))
+
+
+def parse(s):
+    if match := re.search(r'src="(?:https?://)?(?:www.)?youtube.com/embed/(\w{11})"', s):
+        link = match.group(1)
+        return f"https://youtu.be/{link}"
+
+
+if __name__ == "__main__":
+    main()
